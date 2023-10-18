e916fd43afd5cfccd319bb56acad9abcdf77b6f8
iamAlinaaa <alinaaevns1806@gmail.com>
2023-10-18 18:14:04 +0300

    added functionality

--- src/components/task-item/task-item.tsx
+++ src/components/task-item/task-item.tsx
@@ -1,43 +1,8 @@
-// import React from "react";
-// import type { Task } from "../../slices/task-slice";
-// import { useDispatch } from "react-redux";
-// import { actions } from "../../slices/task-slice";
-
-// type TaskItemProps = {
-//   task: Task;
-// };
-
-// const TaskItem: React.FC<TaskItemProps> = ({ task }) => {
-//   const dispatch = useDispatch();
-
-//   const handleRemove = () => {
-//     dispatch(actions.removeTask(task.id));
-//   };
-
-//   const handleToggleStatus = () => {
-//     dispatch(actions.toggleTaskStatus(task.id));
-//   };
-
-//   return (
-//     <div>
-//       <h3>{task.title}</h3>
-//       <p>{task.description}</p>
-//       <p>Priority: {task.priority}</p>
-//       <p>Status: {task.status}</p>
-//       <button onClick={handleToggleStatus}>Toggle Status</button>
-//       <button onClick={handleRemove}>Remove</button>
-//     </div>
-//   );
-// };
-
-// export { TaskItem };
-
-
 import React from 'react';
 import type { Task } from '../../slices/task-slice';
 import { useDispatch } from 'react-redux';
 import { actions } from '../../slices/task-slice';
-import styles from './styles.module.css'; // Import your styles
+import styles from './styles.module.css'; 
 
 type TaskItemProps = {
   task: Task;
@@ -56,8 +21,8 @@ const TaskItem: React.FC<TaskItemProps> = ({ task }) => {
 
   return (
     <div className={styles['task-item']}>
-      <h3>{task.title}</h3>
-      <p>{task.description}</p>
+      <h3>{task.title !== '' ? task.title : 'Add your title'}</h3>
+      <p>{task.description !== '' ? task.description : 'Add your description'}</p>
       <p>Priority: {task.priority}</p>
       <p>Status: {task.status}</p>
       <button onClick={handleToggleStatus}>Toggle Status</button>
