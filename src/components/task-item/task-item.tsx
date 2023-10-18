b68ea6ffa6a3840227291da153a12d9cfa0756a6
iamAlinaaa <alinaaevns1806@gmail.com>
2023-10-18 21:39:37 +0300

    functionality improvements

--- src/components/task-item/task-item.tsx
+++ src/components/task-item/task-item.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
-import type { Task } from '../../slices/task-slice';
-import { useDispatch } from 'react-redux';
-import { actions } from '../../slices/task-slice';
-import styles from './styles.module.css'; 
+import React from "react";
+import type { Task } from "../../slices/task-slice";
+import { useDispatch } from "react-redux";
+import { actions } from "../../slices/task-slice";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleCheck, faTrashCan } from "@fortawesome/free-solid-svg-icons";
+import styles from "./styles.module.css";
 
 type TaskItemProps = {
   task: Task;
@@ -20,13 +22,19 @@ const TaskItem: React.FC<TaskItemProps> = ({ task }) => {
   };
 
   return (
-    <div className={styles['task-item']}>
-      <h3>{task.title !== '' ? task.title : 'Add your title'}</h3>
-      <p>{task.description !== '' ? task.description : 'Add your description'}</p>
-      <p>Priority: {task.priority}</p>
-      <p>Status: {task.status}</p>
-      <button onClick={handleToggleStatus}>Toggle Status</button>
-      <button onClick={handleRemove}>Remove</button>
+    <div className={styles["task-item"]}>
+      <h3>{task.title !== "" ? task.title : "Add your title"}</h3>
+      <p>
+        {task.description !== "" ? task.description : "Add your description"}
+      </p>
+      <p>{task.priority} Priority</p>
+      <p>{task.status}</p>
+      <div onClick={handleToggleStatus}>
+        <FontAwesomeIcon icon={faCircleCheck} />
+      </div>
+      <div onClick={handleRemove}>
+        <FontAwesomeIcon icon={faTrashCan} />
+      </div>
     </div>
   );
 };
